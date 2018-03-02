b4f900974c901af33937c955e66f9bbb0dd39e3c
bryantwofourtwo <bryantwofourtwo@gmail.com>
2018-03-01 21:14:13 -0700

    added .

--- abkrakn/src/components/SearchBar/SearchBar.js
+++ abkrakn/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./SearchBar.css";
+
 const SearchBar = () => (
     <div className="container">
-    <div className="input-group mb-8">
+    <div className="input-group mb-3">
         <div className="input-group-prepend">
-            <span className="input-group-text" id="inputGroup-sizing-small">Search</span>
+            <button className="btn btn-outline-dark" type="button">Search</button>
         </div>
         <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
     </div>
