28b87c30f89716406250483209d15356a7a35954
bryantwofourtwo <bryantwofourtwo@gmail.com>
2018-03-01 20:34:33 -0700

    added code

--- abkrakn/src/components/Footer/Footer.js
+++ abkrakn/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => (
     <footer className="footer">
         <div className="container footer1">
-            <span className="texty">Copyright 2018</span>
+            <span className="texty">Copyright &copy; 2018</span>
         </div>
 </footer>
 );
