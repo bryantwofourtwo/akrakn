1826ffcbfc750a91d0978ebcf5f3e75e4d0e2036
bryantwofourtwo <bryantwofourtwo@gmail.com>
2018-03-01 21:21:31 -0700

    moar added

--- abkrakn/src/components/EnterInfo/EnterInfo.js
+++ abkrakn/src/components/EnterInfo/EnterInfo.js
@@ -30,30 +30,32 @@ class EnterInfo extends Component {
     <div className="container" id="enterInfo">
         <form>
             <div className="form-row">
-                <div className="form-group col-md-6">
+                <div className="form-group col-md-8">
                     <label>Email</label>
                     <input type="email" className="form-control" name="inputEmail4" value={this.state.inputEmail4} placeholder="example@aol.com" onChange={this.handleChange}/>
                 </div>
-                <div className="form-group col-md-6">
+                <div className="form-group col-md-8">
                     <label>Password</label>
                     <input type="password" className="form-control" name="inputPassword4" value={this.state.inputPassword4}  onChange={this.handleChange} placeholder="*****" />
                 </div>
             </div>
-                <div className="form-group col-md-6">
+                <div className="form-group col-md-8">
                     <label>Enter Item Number</label>
                     <input type="text" className="form-control" name="inputItemNumber" value={this.state.inputItemNumber}  onChange={this.handleChange} placeholder="ASIN #" />
+                    <br />
                     <button className="btn btn-outline-dark" onClick={this.handleClick}><span>SEARCH</span></button>
                 </div>
                 <div className="form-row">
-                    <div className="col-md-6">
+                    <div className="col-md-8">
                     <input type="text" className="form-control" placeholder="First Name" />
                     </div>
-                    <div className="col-md-6">
-                    <input type="text" className="form-control" placeholder="Last Name" />
+                    <br />
+                    <div className="col-md-8">
+                    <input type="text" className="form-control" placeholder="Last Name" />                
+                    <br />
+                    <button type="submit" className="btn btn-outline-dark">Submit</button>
                     </div>
-                </div>
-
-            <button type="submit" className="btn btn-outline-dark">Submit</button>
+                </div>            
         </form>
     </div>)
     }
--- abkrakn/src/components/SearchBar/SearchBar.js
+++ abkrakn/src/components/SearchBar/SearchBar.js
@@ -3,11 +3,11 @@ import "./SearchBar.css";
 
 const SearchBar = () => (
     <div className="container">
-    <div className="input-group mb-3">
+    <div className="col-md-8">
         <div className="input-group-prepend">
             <button className="btn btn-outline-dark" type="button">Search</button>
         </div>
-        <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
+        <input type="text" className="form-control" placeholder="global search" />
     </div>
     </div>
 
